9191edbd1f93a382f6d6eb436acfa4b5c98095e0
CompSciHigh24 <csh-github@compscihigh.org>
2024-12-18 04:24:00 -0500

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,51 @@
+const express = require("express")
+const apple = express()
+
+const animalShelterData = {
+    cats: [
+        { name: "Whiskers", age: 2, shelter: "Happy Tails Shelter" },
+        { name: "Mittens", age: 3, shelter: "Cozy Paws Sanctuary" },
+        { name: "Shadow", age: 1, shelter: "Happy Tails Shelter" }
+    ],
+    shelters: [
+        { name: "Happy Tails Shelter", location: "123 Main Street, Cityville" },
+        { name: "Cozy Paws Sanctuary", location: "456 Oak Avenue, Townsburg" }
+    ]
+};
+
+app.use((request, response, next) => {
+    console.log(request.method + "/" + request.url)
+    next()
+})
+
+app.get("/", (request, response) => {
+    response.send("<h1>Welcome to the Animal Shelter Network </h1>")
+})
+
+app.get("/api/cat", (request, response) => {
+    response.json(animalShelterData.cats)
+})
+
+app.get("/api/shelters", (request, response) => {
+    response.json(animalShelterData.shelters)
+})
+
+app.get("/docs", (request, response) => {
+    response.send("Go to /api/cat to see cats for adoption and /api/shelters to see shelters in the area")
+})
+
+app.get("/adopt/cat", (request, response) => {
+    response.send("A cat you can adopt is " + animalShelterData.cats[0].name)
+})
+
+app.use((request, response, next) => {
+    response.status(404).send("404 NOT FOUND")
+})
+
+app.use((request, response, next) => {
+    response.status(404).send("<h1>404 NOT FOUND </h1>")
+})
+
+app.listen(3000, () => {
+    console.log("Server is running")
+})
